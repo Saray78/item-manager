d8dfcd48d8c31fbc332ad4b80242ba34b903db70
saray <saray.balbuena@clarivate.com>
2020-08-17 16:38:23 +0200

    * Fix sorting and favorites * Tests

--- src/app/components/item-card/item-card.component.ts
+++ src/app/components/item-card/item-card.component.ts
@@ -11,7 +11,7 @@ import { ItemSearchMode } from '../../models/item-search-model/item-search.model
 })
 export class ItemCardComponent {
   @Input() itemCardData: ItemCardModel[];
-  @Input() AllItemCardData: ItemCardModel[];
+  @Input() itemCardDataList: ItemCardModel[];
   @Input() itemCardDataMode: string;
   readonly ITEM_SEARCH_BASIC_MODE: string = ItemSearchMode.basicMode;
 
@@ -25,6 +25,6 @@ export class ItemCardComponent {
   }
 
   private saveFavoriteItem(): void {
-    this.favoriteItemsService.saveFavoriteItems(this.AllItemCardData);
+    this.favoriteItemsService.saveFavoriteItems(this.itemCardDataList);
   }
 }
